4da5a127728ec27bb7aedcf4069c7cc5e892bd8f
Maksym Sobolyev <sobomax@sippysoft.com>
2019-12-28 08:50:38 -0800

    Go back to using "long long" temporary. Looks like it's much safer, typeof is not part of the C++ standard (sadly).

--- src/timespecops.h
+++ src/timespecops.h
@@ -69,21 +69,20 @@
         NSEC(tp) = round((double)NSEC_IN_SEC * ((d) - SEC(tp)));   \
     } while (0)
 
-#define timespecmul(rvp, vvp, uvp)                                    \
-    do {                                                              \
-        typeof(NSEC(rvp)) tnsec;                                      \
-        typeof(SEC(rvp)) tsec;                                        \
-        tsec = SEC(vvp) * SEC(uvp);                                   \
-        tnsec = (typeof(tnsec))(NSEC(vvp) * NSEC(uvp)) / NSEC_IN_SEC; \
-        tnsec += (typeof(tnsec))(SEC(vvp) * NSEC(uvp));               \
-        tnsec += (typeof(tnsec))(SEC(uvp) * NSEC(vvp));               \
-        SEC(rvp) = tsec;                                              \
-        if (tnsec >= NSEC_IN_SEC) {                                   \
-            SEC(rvp) += (tnsec / NSEC_IN_SEC);                        \
-            NSEC(rvp) = (tnsec % NSEC_IN_SEC);                        \
-        } else {                                                      \
-            NSEC(rvp) = tnsec;                                        \
-        }                                                             \
+#define timespecmul(rvp, vvp, uvp)                                \
+    do {                                                          \
+        long long tsec, tnsec;                                    \
+        tsec = SEC(vvp) * SEC(uvp);                               \
+        tnsec = (long long)(NSEC(vvp) * NSEC(uvp)) / NSEC_IN_SEC; \
+        tnsec += (long long)(SEC(vvp) * NSEC(uvp));               \
+        tnsec += (long long)(SEC(uvp) * NSEC(vvp));               \
+        SEC(rvp) = tsec;                                          \
+        if (tnsec >= NSEC_IN_SEC) {                               \
+            SEC(rvp) += (tnsec / NSEC_IN_SEC);                    \
+            NSEC(rvp) = (tnsec % NSEC_IN_SEC);                    \
+        } else {                                                  \
+            NSEC(rvp) = tnsec;                                    \
+        }                                                         \
     } while (0)
 
 #endif /* _TIMESPECOPS_H */
